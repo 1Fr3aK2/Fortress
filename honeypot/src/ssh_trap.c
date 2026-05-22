31d2d058ed5c64b2fbfe7ad2aead752953597d33
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2026-05-22 05:03:34 +0100

    changed to accept any IP

--- honeypot/src/ssh_trap.c
+++ honeypot/src/ssh_trap.c
@@ -30,7 +30,7 @@ int setup_Server(int port)
     ft_bzero(&servaddr, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(2130706433);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(port);
 
     if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
