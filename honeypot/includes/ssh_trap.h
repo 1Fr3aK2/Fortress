8128bc1730009d13efe09712240b6f9b0d486201
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2026-05-22 04:52:25 +0100

    added libraries and implemented TCP server at 2222 port

--- honeypot/includes/ssh_trap.h
+++ honeypot/includes/ssh_trap.h
@@ -6,5 +6,44 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/epoll.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <../libraries/libft/libft.h>
+
+#define MAX_CLIENTS 1024
+#define MAX_MSG_SIZE 1000000
+
+typedef struct s_client
+{
+    int id;
+    char msg[MAX_MSG_SIZE];
+} t_client;
+
+t_client clients[MAX_CLIENTS];
 
 
+int maxfd = 0;
+int current_id = 0;
+
+char recv_buffer[MAX_MSG_SIZE];
+char send_buffer[MAX_MSG_SIZE + 50];
+
+fd_set write_set;
+fd_set read_set;
+fd_set current;
+
+
+//functions
+
+void err(char *msg);
+void send_Broadcast(int accepted);
+
+int setup_Server(int port);
+void handle_NewConnections(int sockfd);
+void handle_Clients(int fd);
+void handle_ClientMessage(int fd, int ret);
+void run_Server(int sockfd);
+#endif
---
+++ honeypot/src/ssh_trap.c
@@ -0,0 +1,126 @@
+#include <ssh_trap.h>
+
+void err(char *msg)
+{
+    if (!msg)
+        ft_putstr_fd("Fatal error\n", 2);
+    else
+        ft_putstr_fd(msg, 2);
+    exit(1);
+}
+
+void send_Broadcast(int accepted)
+{
+    for (int fd = 0; fd <= maxfd; fd++)
+    {
+        if (FD_ISSET(fd, &write_set) && fd != accepted)
+            if (send(fd, send_buffer, strlen(send_buffer), 0) == -1)
+                err(NULL);
+    }
+}
+
+int setup_Server(int port)
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+        err(NULL);
+    ft_bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(2130706433);
+    servaddr.sin_port = htons(port);
+
+    if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+        err(NULL);
+    if (listen(sockfd, 10) != 0)
+        err(NULL);
+    return (sockfd);
+}
+
+void handle_NewConnections(int sockfd)
+{
+    struct sockaddr_in client;
+    socklen_t len;
+    int connfd;
+
+    ft_bzero(&client, sizeof(client));
+    len = sizeof(client);
+    connfd = accept(sockfd, (struct sockaddr *)&client, &len);
+    if (connfd < 0)
+        err(NULL);
+    if (connfd > maxfd)
+        maxfd = connfd;
+    clients[connfd].id = current_id;
+    current_id++;
+    FD_SET(connfd, &current);
+    sprintf(send_buffer, "server: client %d just arrived\n", clients[connfd].id);
+    send_Broadcast(connfd);
+}
+
+void handle_Clients(int fd)
+{
+    int ret;
+    ret = recv(fd, recv_buffer, MAX_MSG_SIZE, 0);
+    if (ret <= 0)
+    {
+        sprintf(send_buffer, "server: client %d just left\n", clients[fd].id);
+        send_Broadcast(fd);
+        FD_CLR(fd, &current);
+        close(fd);
+    }
+    else
+        handle_ClientMessage(fd, ret);
+
+}
+
+
+void handle_ClientMessage(int fd, int ret)
+{
+    for (int i = 0, j = ft_strlen(clients[fd].msg); i < ret; i++, j++)
+    {
+        clients[fd].msg[j] = recv_buffer[i];
+        if (clients[fd].msg[j] == '\n')
+        {
+            clients[fd].msg[j] = '\0';
+            sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+            send_Broadcast(fd);
+            ft_bzero(clients[fd].msg, ft_strlen(clients[fd].msg));
+            j = -1;
+        }
+    }
+}
+
+void run_Server(int sockfd)
+{
+    while (1)
+    {
+        read_set = write_set = current;
+        if (select(maxfd + 1, &read_set, &write_set, NULL, NULL) == -1)
+            err(NULL);
+        for (int fd = 0; fd <= maxfd; fd++)
+        {
+            if (FD_ISSET(fd, &read_set))
+            {
+                if (fd == sockfd)
+                    handle_NewConnections(sockfd);
+                else
+                    handle_Clients(fd);
+            }
+        }
+    }
+}
+
+int main()
+{    
+    int sockfd;
+
+    sockfd = setup_Server(2222);
+    FD_ZERO(&current);
+    FD_SET(sockfd, &current);
+    maxfd = sockfd;
+
+    run_Server(sockfd);
+}
